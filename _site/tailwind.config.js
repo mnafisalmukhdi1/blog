bf315f787abd0315df9d92abc83ac0723b74846d
M. Nafis Al-Mukhdi <contact.mnam@gmail.com>
2024-07-22 07:07:49 +0800

    update

---
+++ _site/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // ...
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ]
+}
