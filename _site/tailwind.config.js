aa167f2eb986433721f6972a80eed661c227174c
M. Nafis Al-Mukhdi <contact.mnam@gmail.com>
2024-07-22 11:13:42 +0800

    update interface

--- _site/tailwind.config.js
+++ _site/tailwind.config.js
@@ -1,9 +1,16 @@
+const plugin = require('tailwindcss/plugin')
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  // ...
+  content: [],
+  theme: {
+    extend: {},
+  },
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
-  ]
+    require('@tailwindcss/container-queries'),
+  ],
 }
+
